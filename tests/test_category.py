16fd84072e20412759d92798e628f27ba7636eeb
elenazaharova <elena.zakharova@simbirsoft.com>
2024-11-16 16:47:38 +0300

    15.1 fix 3

--- tests/test_category.py
+++ tests/test_category.py
@@ -1,7 +1,23 @@
-# создание теста для класса Category
 def test_category_init(first_category):
     assert first_category.name == "Смартфоны"
     assert first_category.description == "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни"
-    assert len(first_category.products) == 3
+    assert len(first_category.products_list) == 3
     assert first_category.category_count == 1
     assert first_category.product_count == 3
+
+def test_products(products):
+   assert products == "Samsung Galaxy S23 Ultra, 180000.0 руб. Остаток: 5 шт."
+
+def test_new_product(add_product):
+    assert add_product == "Samsung Galaxy S23 Ultra\n 256GB, Серый цвет, 200MP камера\n 180000.0\n 5"
+
+def test_price(price):
+    assert price == "Изменять цену? Введите y если да,и n если нет."
+
+def test_category_with_quantity(category_with_quantity):
+   assert category_with_quantity == "Cмартфоны, количество продуктов: 27шт"
+
+
+
+
+
--- tests/test_product.py
+++ tests/test_product.py
@@ -4,3 +4,14 @@ def test_product_init(product):
     assert product.description == "256GB, Серый цвет, 200MP камера"
     assert product.price == 180000.0
     assert product.quantity == 5
+
+def test_str(product):
+    assert product.name == "Samsung Galaxy S23 Ultra"
+    assert product.price == 180000.0
+    assert product.quantity == 5
+
+def test_product_with_quantity(product_with_quantity):
+   assert product_with_quantity == "Samsung Galaxy S23 Ultra, 180000.0 руб. Остаток: 5 шт."
+
+def test_product_count(product_count):
+   assert product_count == "2580000.0"
