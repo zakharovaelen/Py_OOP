9f3d5a9400d994821a2e379ef536b4e8bab56f9c
elenazaharova <elena.zakharova@simbirsoft.com>
2025-01-05 18:49:55 +0300

    16.2 fix 3

--- src/16.2_main.py
+++ src/16.2_main.py
@@ -1,9 +1,12 @@
+from src.category import Category
+
+from src.product import Product
 
 
 if __name__ == '__main__':
-    product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
-    product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
-    product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
+    product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5, "red")
+    product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8, "red")
+    product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14, "red")
 
     print(product1.name)
     print(product1.description)
@@ -30,7 +33,7 @@ if __name__ == '__main__':
     print(category1.category_count)
     print(category1.product_count)
 
-    product4 = Product("55\" QLED 4K", "Фоновая подсветка", 123000.0, 7)
+    product4 = Product("55\" QLED 4K", "Фоновая подсветка", 123000.0, 7, "red")
     category2 = Category("Телевизоры",
                          "Современный телевизор, который позволяет наслаждаться просмотром, станет вашим другом и помощником",
                          [product4])
